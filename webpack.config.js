fb53c820f8b76d06e77c49de0c70bf670310e05f
unknown <babuldeb101@gmail.com>
2021-07-06 09:14:39 +0000

    added htmlwebpackplugin and cleanhtmlwebpackplugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
+const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
 let mode = "development";
 let target = "web";
 
@@ -10,6 +14,7 @@ module.exports = {
   mode: mode,
   target: target,
   output: {
+    path: path.resolve(__dirname, "public"),
     assetModuleFilename: "images/[hash][ext][query]",
   },
 
@@ -41,7 +46,13 @@ module.exports = {
     ],
   },
 
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
   resolve: {
     extensions: [".js", ".jsx"],
   },
